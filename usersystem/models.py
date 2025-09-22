27db995bb31e83c0008d97d63a8413571ea79a93
shuxuan1 <shuxuan.wang1@student.unimelb.edu.au>
2025-09-22 22:42:07 +1000

    register success

--- usersystem/models.py
+++ usersystem/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Usersystem(models.Model):
-
-    Username = models.CharField(max_length=100)
-    Password = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
 
 
--- usersystem/views.py
+++ usersystem/views.py
@@ -3,6 +3,8 @@ from .models import Usersystem
 from .forms import LoginForm,RegisterForm
 from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
 # Create your views here.
 def login(request):
     if request.method == 'GET':
@@ -21,18 +23,19 @@ def register(request):
     if request.method == 'GET':
         return render(request, 'register.html')
     else:
-        form = RegisterForm(request.POST)
+        form = RegisterForm(request.POST or None)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             confirmation = form.cleaned_data.get('confirmpassword')
-            Usersystem.objects.create(username=username, password=password, confirmpassword=confirmation)
-            return render(request, 'register.html')
+            User.objects.create_user(username=username, password=password)
+            return redirect('usersystem:login')
         else:
+            print(form.errors)
             return render(request, 'register.html', {'form': form})
 
 def add_user(request):
-    newuser = Usersystem(username='shuxuan1',Password='Guardian1127')
+    newuser = Usersystem(username='shuxuan1',password='Guardian1127')
     newuser.save()
     return HttpResponse('Create account successfully!')
 
@@ -43,10 +46,10 @@ def query_user(request):
     ## search user with feature
     # users = Usersystem.objects.filter(username='shuxuan1')
     for user in users:
-        print(user.username, user.Password)
+        print(user.username, user.password)
     # try:
     #     user = Usersystem.objects.get(username='shuxuan1111')
-    #     print(user.Password)
+    #     print(user.password)
     # except Usersystem.DoesNotExist:
     #     return HttpResponse("User not found!")
 
@@ -54,7 +57,7 @@ def query_user(request):
 
 def update_user(request):
     newuser = Usersystem.objects.get(username='shuxuan1')
-    newuser.Password = ''
+    newuser.password = ''
     newuser.save()
     return HttpResponse("Update user successfully!")
 
